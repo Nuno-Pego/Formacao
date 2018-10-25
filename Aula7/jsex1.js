951d4c4b1a475bb090eef85fe6223c604db7f9e7
Nuno-Pego <44403731+Nuno-Pego@users.noreply.github.com>
2018-10-25 02:13:01 +0100

    falta emendar umas coisas

--- Aula7/jsex1.js
+++ Aula7/jsex1.js
@@ -1,30 +1,66 @@
 
+
 $(document).ready(function () {
 
     var i = 0;
+    x = [];
+    z = [];
 
-    $("#vin np").click(function () {
+    $("#vinp").click(function () {
 
         var ipt = $("#vinpt").val();
+
         console.log(ipt);
 
-        //$('#vtb').html(notas(ipt));
+        x[i] = parseInt(ipt);
+        z = x[0];
+
+        i++;
+
+        $('#p1').html(para(i, z));
+        $('#tb1').html(tabnotas(i, x, z));
+
+
 
     })
 
-    function tabnotas(num) {
+    function para(al, num) {
 
-        if (i == 0) {
+        console.log(al);
+        console.log(num);
 
-        } else
-           
-        i++;
+        var pr = "";
 
-        return tx;
+        if (al <= num) {
+
+            pr = `Indique a nota do ${al}º aluno`;
+
+        }
+
+        return pr;
+    }
+
+    function tabnotas(alu, nt, nume) {
+
+        console.log(nt);
+
+        var h = alu - 1;
+
+        txt = "";
+
+        if (nt.length == 1) {
+
+        } else if (nt.length <= nume + 2) {
+
+            txt = ` <td> ${h} </td>
+                    <td> ${nt[alu]} </td>`;
+
+        }
+        return txt;
+
     }
 
 })
-
 
 
 
