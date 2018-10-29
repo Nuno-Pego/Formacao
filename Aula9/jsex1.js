224f37dc165315e5ae526f5fc068f4745b831727
Nuno-Pego <44403731+Nuno-Pego@users.noreply.github.com>
2018-10-29 22:30:37 +0000

    ponte entre input e functions

--- Aula9/jsex1.js
+++ Aula9/jsex1.js
@@ -1,6 +1,5 @@
 
 
-
 $(document).ready(function () {
 
     $("#clka").click(function () {
@@ -24,3 +23,34 @@ $(document).ready(function () {
 
 })
 
+function novaFactura(cliente,artigo,extra){
+    return new Factura(cliente,artigo,extra);
+}
+
+function salvFactura(factura){
+    data.push(factura);
+}
+
+function displayFacturaByID(id){
+    for (let i = 0; i < data.length; i++) {
+        const factura = orderData()[i];
+        if (factura.id == id){
+            return factura;
+        }   
+    }
+    return "Nao existe!"; 
+}
+
+function removeFacturaByID(id){
+    for (let j = 0; j < data.length; j++) {
+        const factura = orderData()[j];
+        if (factura.id == id) {
+            data.splice(j,1);
+        }
+    }
+    return "Nao existe!";
+}
+
+function orderData(){
+    return data.sort();
+}
