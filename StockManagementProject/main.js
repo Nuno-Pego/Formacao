781667e1632d4ab42aa31c2ea9e7680e709dfb47
Nuno <nma_pego@hotmail.com>
2018-11-09 17:48:12 +0000

    continuar

--- StockManagementProject/main.js
+++ StockManagementProject/main.js
@@ -3,17 +3,54 @@ $(document).ready(function () {
     getProducts();
 })
 
- 
+
 function getProducts() {
     $.ajax({
         url: "https://mcupacademy.herokuapp.com/api/Products",
         type: 'GET',
         contentType: 'aplication/json',
-        success: function (data) {            
-            console.log(data);
+        success: function (data) {
+            console.log(data)
+            createTable(data);
         },
         error: function (data) {
             console.log("Error ", data);
         }
     });
+}
+
+
+function createTable(data) {
+
+    createHeader(data);
+
+    for (let j = 0; j < data.length; j++) {
+        const element = data[j];
+
+        txt = `<tr>
+            <td>${element.discountValue}</td>
+            <td>${element.id}</td>
+            <td>${element.iva}</td>
+            <td>${element.pvp}</td>
+            <td><a title="Edit" data-original-title="Edit" href="#"><i class="fas fa-edit"></i></a> <a
+            title="Delete" data-original-title="Delete" href="#"><i class="fas fa-trash-alt"></i></a></td>`;
+
+        $("tbody").append(txt);
+    }
+}
+
+function createHeader(data) {
+
+    var keys = Object.keys(data[0]);
+
+    for (let j = 0; j < data.length; j++) {
+        const element = keys[j];
+
+        txt = `<tr>
+            <th>Discont</td>
+            <th>IVA</td>
+            <th>PVP</td>
+            <th>ID</td>`;
+    }
+    $("thead").append(txt);
 }
