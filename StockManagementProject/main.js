a35a91687b4e5a69a2f03fa12b84ca896b3b98a0
Nuno <nma_pego@hotmail.com>
2018-11-12 17:37:17 +0000

    tabelas feitas

--- StockManagementProject/main.js
+++ StockManagementProject/main.js
@@ -1,7 +1,42 @@
 
+$("#showtable1").hide();
+$("#showtable2").hide();
+
+var ctrl;
+
 $(document).ready(function () {
-    // getProducts();
-    getShelves();
+
+    $("#ClkAll").click(function () {
+        $("#tbProduct tbody").remove();
+        $("#tbShelves tbody").remove();
+        $("#showtable1").attr("class","col-sm-5 col-md-5");
+        $("#showtable2").attr("class","col-sm-5 col-md-5");  
+        ctrl = false;
+        getProducts();
+        getShelves();
+
+    })
+
+    $("#ClkProducts").click(function () {
+        $("#tbProduct tbody").remove();
+        $("#tbShelves tbody").remove();
+        $("#showtable2").hide();
+        $("#showtable1").attr("class","col-sm-10 col-md-10");        
+        ctrl = true;
+        getProducts();
+
+    })
+
+    $("#ClkShelfs").click(function () {
+        $("#tbProduct tbody").remove();
+        $("#tbShelves tbody").remove();
+        $("#showtable1").hide();
+        $("#showtable1").hide();
+        $("#showtable2").attr("class","col-sm-10 col-md-10"); 
+        ctrl = true;
+        getShelves();
+
+    })
 })
 
 
@@ -11,8 +46,9 @@ function getProducts() {
         type: 'GET',
         contentType: 'aplication/json',
         success: function (data) {
-            console.log(data)
-            createTable(data);
+            console.log(data);
+            $("#showtable1").show();
+            createTableProduct(data);
         },
         error: function (data) {
             console.log("Error ", data);
@@ -27,7 +63,9 @@ function getShelves() {
         type: 'GET',
         contentType: 'aplication/json',
         success: function (data) {
-            console.log(data)            
+            console.log(data);
+            $("#showtable2").show();
+            createTableShelves(data);
         },
         error: function (data) {
             console.log("Error ", data);
@@ -36,37 +74,46 @@ function getShelves() {
 }
 
 
-function createTable(data) {
-
-    createHeader(data);
+function createTableProduct(data) {
 
     for (let j = 0; j < data.length; j++) {
         const element = data[j];
 
-        txt = `<tr>
+        var txt1 = `<tbody>
+            <tr>
             <td>${element.discountValue}</td>
             <td>${element.id}</td>
             <td>${element.iva}</td>
-            <td>${element.pvp}</td>
-            <td><a title="Edit" data-original-title="Edit" href="#"><i class="fas fa-edit"></i></a> <a
-            title="Delete" data-original-title="Delete" href="#"><i class="fas fa-trash-alt"></i></a></td>`;
-
-        $("tbody").append(txt);
+            <td>${element.pvp}</td>`;
+        if (ctrl == true) {
+            var txt2 = `<td><a title="Edit" data-original-title="Edit" href="#"><i class="fas fa-edit"></i></a> <a
+                title="Delete" data-original-title="Delete" href="#"><i class="fas fa-trash-alt"></i></a></td></tr></tbody>`;
+        }else{
+            var txt2=`</tr></tbody>`;
+        }
+        $("#tbProduct").append(txt1 + txt2);
     }
 }
 
-function createHeader(data) {
-
-    var keys = Object.keys(data[0]);
+function createTableShelves(data) {
 
     for (let j = 0; j < data.length; j++) {
-        const element = keys[j];
+        const element = data[j];
 
-        txt = `<tr>
-            <th>Discont</td>
-            <th>IVA</td>
-            <th>PVP</td>
-            <th>ID</td>`;
+        var txt1 = `<tbody>
+            <tr>
+            <td>${element.capacity}</td>
+            <td>${element.id}</td>
+            <td>${element.productId}</td>
+            <td>${element.rentPrice}</td>`;
+
+        if (ctrl == true) {
+            var txt2 = `<td><a title="Edit" data-original-title="Edit" href="#"><i class="fas fa-edit"></i></a> <a
+                    title="Delete" data-original-title="Delete" href="#"><i class="fas fa-trash-alt"></i></a></td></tr></tbody>`;
+        }else{
+            var txt2=`</tr></tbody>`;
+        }
+        $("#tbShelves").append(txt1 + txt2);
     }
-    $("thead").append(txt);
 }
+
